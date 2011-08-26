5f14c0dfbfc895bbdaedf2526b2641d089d25bce
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-08-25 21:21:11 -0400

    added new include paths

--- public/index.php
+++ public/index.php
@@ -23,27 +23,36 @@ echo
 			</div>
 		<div id ="content">';
 // to do move cacher here ...
+
  if(!$_POST['searchTerm']){
          if($_GET['u'])
                 $_POST['u'] = $_GET['u'];
          elseif($_GET['r'])
                 $_POST['r'] = $_GET['r'];
   }
- if($_POST['searchTerm'] || $_GET['r'] || $_GET['u'])   include("../lib/rxNormRef.php");
+ if($_POST['searchTerm'] || $_GET['r'] || $_GET['u']) {
+ 	
+ 	require("../config.php");
+	require("../lib/obcer.php");
+ //	obcer::ob_cacher();
+ 	
+ 	include("../lib/rxNormRef.php");
 
-?>
+}
+
+echo"
 		</div>
 				<div id = 'help'>
 				<ul>
-					<li><img class='bg_black' src="img/nlm-logo-white.png" alt = "National Library of Medicine Logo" title="NLM Logo" align="middle"/>
+					<li><img class='bg_black' src='img/nlm-logo-white.png' alt = 'National Library of Medicine Logo' title='NLM Logo' align='middle'/>
 						<em>What is RxNix</em>
-						<p>RxNix is a simple semantic medications tool that intefaces with RxNorm - a database developed at the <a href="http://www.nlm.nih.gov/">National Library of Medicine</a>.</p>
-						<a href="http://www.nlm.nih.gov/research/umls/rxnorm/index.html">About RxNorm</a>
+						<p>RxNix is a simple semantic medications tool that intefaces with RxNorm - a database developed at the <a href='http://www.nlm.nih.gov/'>National Library of Medicine</a>.</p>
+						<a href=;http://www.nlm.nih.gov/research/umls/rxnorm/index.html'>About RxNorm</a>
 					</li>
 						
-					<li><img src="img/UMLS_header_newtree.gif" alt= "Unified Medication Language System" title="UMLS Logo" align="middle"/>
+					<li><img src='img/UMLS_header_newtree.gif' alt= 'Unified Medication Language System' title='UMLS Logo' align='middle'/>
 						<em>UMLS</em>
-						<p>RxNorm uses the <a href="http://www.nlm.nih.gov/research/umls/">Unified Medication Language System</a> to enable a variety of systems to communicate with each other.</p> <a href="http://www.nlm.nih.gov/research/umls/quickstart.html">UMLS Quickstart Guide</a>
+						<p>RxNorm uses the <a href='http://www.nlm.nih.gov/research/umls/'>Unified Medication Language System</a> to enable a variety of systems to communicate with each other.</p> <a href='http://www.nlm.nih.gov/research/umls/quickstart.html'>UMLS Quickstart Guide</a>
 					</li>
 						
 					<li>
@@ -53,12 +62,12 @@ echo
 			
 					<li>
 					<em>About</em>
-						<p>Built with <a href="https://github.com/codeforamerica/rxNorm_php">rxNorm_php</a> api library and <a href="https://github.com/codeforamerica/rxNormRef_php">rxNormRef_php</a> maintained  by <a href="http://codeforamerica.org">Code For America.</a>
+						<p>Built with <a href='https://github.com/codeforamerica/rxNorm_php'>rxNorm_php</a> api library and <a href='https://github.com/codeforamerica/rxNormRef_php'>rxNormRef_php</a> maintained  by <a href='http://codeforamerica.org'>Code For America.</a>
 							</p>
-							<img src ="http://www.totalvalidator.com/images/valid_s_us508.gif" alt="Total Validator Badge" title="Validation Badge"/>
+							<img src ='http://www.totalvalidator.com/images/valid_s_us508.gif' alt='Total Validator Badge' title='Validation Badge'/>
 					</li>
 				</ul>
 			</div>
 	</div>		
 		</body>
-</html>
+</html>";
