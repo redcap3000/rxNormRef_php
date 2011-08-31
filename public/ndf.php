f0932becb19ce8e2319c7bb289cb78d7bfb82838
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-08-31 05:24:18 -0400

    probably removed access to a feature I am not supporting right now

--- public/ndf.php
+++ public/ndf.php
@@ -27,39 +27,7 @@ if($_GET['n'] ){
 	$_POST['nui'] = $_GET['n'];$_POST['findConcepts'] = 'on';
 }
 
-function html_form($type,$array,$id,$label=true,$legend=true,$blank_item=true,$fieldset=true,$inner_container=NULL){
-// could ditch the type specification and assume that if not provided we are making a checkbox, but if we want to handle other variables
-// we'll have to do something else to tell it its not a checkbox value
-// check array if assoc, if so then use the value as its label
-	if(is_array($array))
-		foreach($array as $loc=>$prop){
-			$select = (!is_int($loc)? $loc: str_replace('_',' ',$prop));
-			switch ($type) {
-			case "option":
-				$result.= '<option value="'.$prop.'" '.($_POST[$id] == $prop? " selected ":NULL).'>'. $select . '</option>';
-				break;
-			case "radio":
-				$result .= '<input type="radio" name="'.$id.'" id="'.$id.'" value="'.$prop.'" '.($_POST[$id] == $prop ? ' checked = "checked" ':NULL).' /> '.($label==true?'<label for= "'.$prop.'">'.$select.'</label>':NULL);
-				break;
-			}
-			if($inner_container !=NULL && is_array($inner_container)) $result .= $inner_container[0] . $result . $inner_container[1];
-			}
-	elseif($type='checkbox' && is_string($array))
-	// this can't use won't need an inner array... also support an array of checkboxes (given an assoc array)
-	// if the count of the passed items is equal to the default then automatically set label/legend/blank item / fieldset to false
-		 $result .= '<input type="checkbox" name="'.$id.'" id="'.$id.'" '.($_POST[$id] != 'on' ?  NULL :' checked = "checked" ' ).'/>';
-		// check box processing
-		return ($fieldset!=true?NULL:'<fieldset>')
-				.
-				($legend!=true?NULL:'<legend>Show '.$id.' value</legend>')
-				.
-				
-				($type=='option'?
-				'<select title="'.$id.'" name="'.$id.'">'.
-					($blank_item != true?NULL:'<option value ="">Select '.$id.'</option>') .$result . '</select>':$result)
-				.
-				($fieldset!=true?NULL:'</fieldset>');
-}
+
 echo 
 		'            
 	</head>
@@ -86,7 +54,7 @@ echo
 						//echo html_form('radio',array('By NUI'=>'byID','By Name'=>'byName'),'findConcepts',true,false);
 						echo '<input type="checkbox" name="findConcepts" id="findConcepts" '.($_POST['findConcepts'] == 'on' ? ' checked = "checked" ':NULL ).'/><label for="findConcepts">Lookup NUI</label></fieldset>';
 						
-						echo '<input type="checkbox" name="advanced" id="advanced" '.($_POST['advanced'] != 'on' ?  NULL :' checked = "checked" ' ).'/><label for="advanced">Advanced Search</label>';
+						//echo '<input type="checkbox" name="advanced" id="advanced" '.($_POST['advanced'] != 'on' ?  NULL :' checked = "checked" ' ).'/><label for="advanced">Advanced Search</label>';
 						
 						echo($_POST['advanced']!='on'?NULL:'<fieldset>'. 
 						'<em>Pick one*</em>
